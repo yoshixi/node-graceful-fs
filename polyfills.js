8840af0f3376783bd0d11b5b79bdee9dc92fecb6
yoshikimasubuchi <yoshixj4@gmail.com>
2022-04-01 12:45:09 +0900

    tmp commit

--- polyfills.js
+++ polyfills.js
@@ -136,7 +136,9 @@ function patch (fs) {
     }
 
     // This ensures `util.promisify` works as it does for native `fs.read`.
-    if (Object.setPrototypeOf && fs$read != undefined) Object.setPrototypeOf(read, fs$read)
+    if (Object.setPrototypeOf) {
+     (fs$read === undefined) ?  Object.setPrototypeOf(read, null) : Object.setPrototypeOf(read, fs$read)
+    }
     return read
   })(fs.read)
 
